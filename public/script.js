192d302888c982aa8fc459f46eed8a3d9a1fb469
elinordin <elin.evelina@live.se>
2021-02-03 16:54:49 +0100

    code cleanup

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+recipesURL = "http://localhost:8080/api/recipes.php"
+pagesURL = "http://localhost:8080/api/pages.php"
 
 //---------------------------------PROGRAM FLOW INDEX---------------------------------
 if (document.querySelector("body").classList.contains("index")) {
@@ -5,7 +7,7 @@ if (document.querySelector("body").classList.contains("index")) {
 
     async function getDataAndDisplayCards() {
         try {
-            let recipesArray = await getRecipeData()
+            let recipesArray = (await getData(recipesURL)).recipes
             displayRecipeCards(recipesArray)
         } catch (error) {
             console.log(error.message)
@@ -17,10 +19,12 @@ if (document.querySelector("body").classList.contains("index")) {
 //---------------------------------PROGRAM FLOW ADMIN---------------------------------
 if (document.querySelector("body").classList.contains("admin")) {
     getDataAndDisplayList()
+    openFormEventListener()
+    addRecipeSubmitListener()
 
     async function getDataAndDisplayList() {
         try {
-            let recipesArray = await getRecipeData()
+            let recipesArray = (await getData(recipesURL)).recipes
             displayRecipeList(recipesArray)
             addDeleteEventListener()
             addEditEventListener(recipesArray)
@@ -29,15 +33,19 @@ if (document.querySelector("body").classList.contains("admin")) {
         }
 
     }
+}
 
-    //---------------------------------EVENT LISTENERS---------------------------------
+//---------------------------------EVENT LISTENERS---------------------------------
+function openFormEventListener () {
     const openFormBtns = Array.from(document.querySelectorAll(".open-form-btn"))
     openFormBtns.forEach(function (btn) {
         btn.addEventListener("click", function(e){
             displayForm(e.target)
         })
     })
+}
 
+function addRecipeSubmitListener () {
     let addRecipeForm = document.querySelector("#add-recipe-form")
     addRecipeForm.addEventListener("submit", function(event) {
         event.preventDefault()
@@ -48,7 +56,7 @@ if (document.querySelector("body").classList.contains("admin")) {
             instructions: addRecipeForm.elements["instructions"].value
         }
 
-        postData("http://localhost:8080/api/recipes.php", inputData)
+        handleData(recipesURL, "POST", inputData)
             .then(response => {
                 alert(response.message)
             })
@@ -57,19 +65,57 @@ if (document.querySelector("body").classList.contains("admin")) {
             })
         location.reload()
     })
+}
 
-    function addDeleteEventListener() {
-        let deleteBtns = document.querySelectorAll(".delete-btn")
-        deleteBtns.forEach(deleteBtn => {
-            deleteBtn.addEventListener("click", function(e){
-                let clickedDeleteBtn = e.target
-                if (e.target.tagName == "I") {
-                    clickedDeleteBtn = e.target.parentNode
+function addDeleteEventListener() {
+    let deleteBtns = document.querySelectorAll(".delete-btn")
+    deleteBtns.forEach(deleteBtn => {
+        deleteBtn.addEventListener("click", function(e){
+            let clickedBtn = getClickedBtn(e.target)
+            let clickedId = {id: clickedBtn.parentNode.id}
+
+            handleData(recipesURL, "DELETE", clickedId)
+                .then(response => {
+                    alert(response.message)
+                })
+                .catch(error => {
+                    alert("You had an error: " + error.message)
+                })
+            location.reload()
+        })
+    })
+}
+
+function addEditEventListener(recipes) {
+    let editBtns = document.querySelectorAll(".edit-btn")
+
+    editBtns.forEach(editBtn => {
+        editBtn .addEventListener("click", function(e){
+            let clickedBtn = getClickedBtn(e.target)
+            let clickedId = clickedBtn.parentNode.id
+
+            let dataToEdit = recipes.find(function(item) {
+                if(item.id === clickedId) {
+                    return true
+                }
+            })
+
+            displayForm(clickedBtn, dataToEdit)
+
+            let editRecipeForm = document.querySelector("#edit-recipe-form")
+            editRecipeForm.addEventListener("submit", function(event) {
+                event.preventDefault()
+
+                let editData = {
+                    id: clickedId,
+                    heading: editRecipeForm.elements["heading"].value,
+                    ingredient: editRecipeForm.elements["ingredient"].value,
+                    instructions: editRecipeForm.elements["instructions"].value
                 }
 
-                let clickedId = {id: clickedDeleteBtn.parentNode.id}
+                console.log(editData)
 
-                deleteData("http://localhost:8080/api/recipes.php", clickedId)
+                handleData(recipesURL,"PUT", editData)
                     .then(response => {
                         alert(response.message)
                     })
@@ -79,73 +125,24 @@ if (document.querySelector("body").classList.contains("admin")) {
                 location.reload()
             })
         })
-    }
-
-    function addEditEventListener(recipes) {
-        let editBtns = document.querySelectorAll(".edit-btn")
-        editBtns.forEach(editBtn => {
-            editBtn .addEventListener("click", function(e){
-                let clickedBtn = e.target
-                if (e.target.tagName == "I") {
-                    clickedBtn = e.target.parentNode
-                }
-
-                let clickedId = clickedBtn.parentNode.id
-                let recipeToEdit = recipes.find(function(recipe) {
-                    if(recipe.id == clickedId) {
-                        return true
-                    }
-                })
-
-                console.log(recipes)
-                console.log(clickedId)
-                console.log(recipeToEdit)
-
-                displayForm(clickedBtn, recipeToEdit)
-
-                let editRecipeForm = document.querySelector("#edit-recipe-form")
-                editRecipeForm.addEventListener("submit", function(event) {
-                    event.preventDefault()
-
-                    let editData = {
-                        id: clickedId,
-                        heading: editRecipeForm.elements["heading"].value,
-                        ingredient: editRecipeForm.elements["ingredient"].value,
-                        instructions: editRecipeForm.elements["instructions"].value
-                    }
-
-                    console.log(editData)
-
-                    updateData("http://localhost:8080/api/recipes.php", editData)
-                        .then(response => {
-                            alert(response.message)
-                        })
-                        .catch(error => {
-                            alert("You had an error: " + error.message)
-                        })
-                    location.reload()
-                })
-            })
-        })
-    }
+    })
 }
+
 
 
 
 
 //---------------------------------FUNCTIONS---------------------------------
-async function getRecipeData() {
-    const response = await fetch("http://localhost:8080/api/recipes.php")
+async function getData(url) {
+    const response = await fetch(url)
         .then(response => response.json())
-        .then(data => data.recipes)
         .catch(error => console.log(error))
     return response
 }
 
-
-async function postData(url="", data={}) {
+async function handleData(url="", method="", data={}) {
     const response = await fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        method: method, // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, *cors, same-origin
         cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
         credentials: "same-origin", // include, *same-origin, omit
@@ -159,62 +156,39 @@ async function postData(url="", data={}) {
     return response.json()
 }
 
-async function deleteData(url="", id={}) {
-    const response = await fetch(url, {
-        method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json"
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(id)
-    });
-    return response.json()
-}
-
-async function updateData(url="", data={}) {
-    const response = await fetch(url, {
-        method: "PUT", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json"
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data)
-    });
-    return response.json()
-}
-
-
-function displayForm(clickedBtn, recipeToEdit) {
+function displayForm(clickedBtn, dataToEdit) {
     let addRecipeForm = document.querySelector("#add-recipe-form")
     let addPageForm = document.querySelector("#add-page-form")
     let editRecipeForm = document.querySelector("#edit-recipe-form")
-
+    let asideHeading = document.querySelector(".aside-heading")
 
     if (clickedBtn.classList.contains("add-recipe-btn")){
         addRecipeForm.style.visibility = "visible"
         addPageForm.style.visibility = "hidden"
         editRecipeForm.style.visibility = "hidden"
+        asideHeading.innerHTML = "Recipes"
     } else if (clickedBtn.classList.contains("add-page-btn")){
         addRecipeForm.style.visibility = "hidden"
         addPageForm.style.visibility = "visible"
         editRecipeForm.style.visibility = "hidden"
+        asideHeading.innerHTML = "Pages"
     } else if (clickedBtn.classList.contains("edit-btn")){
         addRecipeForm.style.visibility = "hidden"
         addPageForm.style.visibility = "hidden"
         editRecipeForm.style.visibility = "visible"
+        asideHeading.innerHTML = "Recipes"
 
-        editRecipeForm.elements["heading"].value = recipeToEdit.heading
-        editRecipeForm.elements["ingredient"].value = recipeToEdit.ingredient
-        editRecipeForm.elements["instructions"].value = recipeToEdit.instructions
+        editRecipeForm.elements["heading"].value = dataToEdit.heading
+        editRecipeForm.elements["ingredient"].value = dataToEdit.ingredient
+        editRecipeForm.elements["instructions"].value = dataToEdit.instructions
     }
+}
+
+function getClickedBtn(clickedBtn) {
+    if (clickedBtn.tagName == "I") {
+        clickedBtn = clickedBtn.parentNode
+    }
+    return clickedBtn
 }
 
 
@@ -234,11 +208,11 @@ function displayRecipeCards(recipes) {
 
 
 function displayRecipeList(recipes) {
-    const recipesList = document.querySelector(".recipes-list")
+    const asideList = document.querySelector(".aside-list")
 
     recipes.forEach(recipe => {
         let listItem = document.createElement("li")
         listItem.innerHTML = `${recipe.heading} <div class="button-wrapper" id=${recipe.id}><button class="delete-btn"><i class="fas fa-trash-alt"></i></button><button class="edit-btn"><i class="far fa-edit"></i></button></div>`
-        recipesList.appendChild(listItem)
+        asideList.appendChild(listItem)
     })
 }
